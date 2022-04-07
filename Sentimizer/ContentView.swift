ae12349f700a797395eee3aa74245c74f806b7a5
Samuel <saginsberg@icloud.com>
2022-04-07 19:58:50 +0200

    Initial Commit

---
+++ Sentimizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sentimizer
+//
+//  Created by Samuel Ginsberg on 07.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sentimizer/SentimizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimizerApp.swift
+//  Sentimizer
+//
+//  Created by Samuel Ginsberg on 07.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SentimizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
