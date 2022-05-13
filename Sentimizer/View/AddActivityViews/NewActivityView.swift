8b7034a6f8363595b41fbad640da08841709767c
Samuel <saginsberg@icloud.com>
2022-05-13 18:14:11 +0200

    save new activities, ui improvements

--- Sentimizer/View/AddActivityViews/NewActivityView.swift
+++ Sentimizer/View/AddActivityViews/NewActivityView.swift
@@ -12,12 +12,10 @@ struct NewActivityView: View {
     @State var textFieldEditing = false
     @FocusState var textFieldFocus: Bool
     
-    @State var nameChosen = false
-    
-    var columns: [GridItem] =
-    [.init(.adaptive(minimum: 40, maximum: 55))]
-    @State var imageBounds: CGSize = CGSize(width: 35, height: 35)
     @State var iconName = ""
+    
+    @Environment(\.dismiss) private var dismiss
+    @State var shouldBeDismissed = false
     
     var body: some View {
         ScrollView {
@@ -26,9 +24,6 @@ struct NewActivityView: View {
                 
                 TextField("Activity name", text: $activityTextFieldText) { editing in
                     textFieldEditing = editing
-                    if editing == false && !activityTextFieldText.isEmpty {
-                        saveNewActivity(for: activityTextFieldText)
-                    }
                 }
                 .padding()
                 .background(K.dayViewBgColor)
@@ -54,63 +49,14 @@ struct NewActivityView: View {
                     }
                 }
                 
-                Group {
-                    if !nameChosen {
-                        Button {
-                            nameChosen = true
-                        } label: {
-                            SentiButton(icon: nil, title: "Next", fontSize: 15, chevron: false)
-                                .frame(width: 150)
-                        }
-                        .disabled(activityTextFieldText.isEmpty)
-                        .opacity(activityTextFieldText.isEmpty ? 0.3 : 1)
-                        .animation(.easeOut, value: activityTextFieldText)
-                        .padding(.top)
-                    } else {
-                        VStack(alignment: .leading) {
-                            Text("Choose your icon")
-                                .font(.senti(size: 20))
-                                .padding(.top)
-                            ForEach(K.defaultIcons, id: \.0.self) { category in
-                                Text(category.0.uppercased())
-                                    .font(.senti(size: 12))
-                                    .padding(.top)
-                                
-                                LazyVGrid(columns: columns, spacing: 5) {
-                                    ForEach(category.1, id: \.self) { icon in
-                                        Group {
-                                            Image(systemName: icon)
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fit)
-                                                .padding(5)
-                                                .overlay(
-                                                    GeometryReader { g in
-                                                        Color.clear
-                                                            .onAppear {
-                                                                imageBounds = g.size
-                                                            }
-                                                    }
-                                                )
-                                                .frame(width: imageBounds.width > imageBounds.height ? imageBounds.width : imageBounds.height,
-                                                       height: imageBounds.width > imageBounds.height ? imageBounds.width : imageBounds.height)
-                                                .background(
-                                                    RoundedRectangle(cornerRadius: 10)
-                                                        .opacity(iconName == icon ? 0.5 : 0)
-                                                        .gradientForeground()
-                                                )
-                                            
-                                                .onTapGesture {
-                                                    iconName = icon
-                                                }
-                                        }
-                                    }
-                                }
-                            }
-                            .padding(.horizontal, 2)
-                        }
-                    }
+                NavigationLink { IconChooser(done: $shouldBeDismissed, iconName: $iconName) } label: {
+                    SentiButton(icon: nil, title: "Next", fontSize: 15, chevron: false)
+                        .frame(width: 150)
                 }
-                .animation(.easeOut, value: nameChosen)
+                .disabled(activityTextFieldText.isEmpty)
+                .opacity(activityTextFieldText.isEmpty ? 0.3 : 1)
+                .animation(.easeOut, value: activityTextFieldText)
+                .padding(.top)
             }
             .onTapGesture {
                 textFieldEditing = false
@@ -125,6 +71,83 @@ struct NewActivityView: View {
             .padding(.bottom)
         }
         .padding(.horizontal, 15)
+        .onChange(of: shouldBeDismissed) { _ in
+            saveNewActivity(for: activityTextFieldText, icon: iconName)
+            dismiss()
+        }
+    }
+    
+    private func saveNewActivity(for activity: String, icon: String) {
+        print(#function)
+    }
+}
+
+struct IconChooser: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @Binding var done: Bool
+    @Binding var iconName: String
+    
+    var columns: [GridItem] =
+    [.init(.adaptive(minimum: 40, maximum: 55))]
+    @State var imageBounds: CGSize = CGSize(width: 35, height: 35)
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                ViewTitle("Choose your icon", fontSize: 30)
+                
+                ForEach(K.defaultIcons, id: \.0.self) { category in
+                    Text(category.0.uppercased())
+                        .font(.senti(size: 12))
+                        .padding(.top)
+                    
+                    LazyVGrid(columns: columns, spacing: 5) {
+                        ForEach(category.1, id: \.self) { icon in
+                            Group {
+                                Image(systemName: icon)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                                    .overlay(
+                                        GeometryReader { g in
+                                            Color.clear
+                                                .onAppear {
+                                                    imageBounds = g.size
+                                                }
+                                        }
+                                    )
+                                    .frame(width: imageBounds.width > imageBounds.height ? imageBounds.width : imageBounds.height,
+                                           height: imageBounds.width > imageBounds.height ? imageBounds.width : imageBounds.height)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .opacity(iconName == icon ? 0.5 : 0)
+                                            .gradientForeground()
+                                    )
+                                    .onTapGesture {
+                                        iconName = icon
+                                    }
+                            }
+                        }
+                    }
+                }
+                .padding(.horizontal, 2)
+                
+                Button {
+                    dismiss()
+                    done = true
+                } label: {
+                    SentiButton(icon: nil, title: "Done", fontSize: 15, chevron: false)
+                        .frame(width: 150)
+                }
+                .padding(.top, 20)
+                .frame(maxWidth: .infinity)
+                .disabled(iconName.isEmpty)
+                .opacity(iconName.isEmpty ? 0.3 : 1)
+                .animation(.easeOut, value: iconName)
+            }
+            .padding(.horizontal, 15)
+        }
     }
     
     struct IconBackground: View {
@@ -142,10 +165,6 @@ struct NewActivityView: View {
                     .gradientForeground()
             }
         }
-    }
-    
-    private func saveNewActivity(for activity: String) {
-        print(#function)
     }
 }
 
