d6c98fd8cb3aa76662d058e71dab5d54d69aad10
Samuel <saginsberg@icloud.com>
2022-05-14 18:47:42 +0200

    fix scroll bug when keyboard appears

--- Sentimizer/View/ActivityDetailView.swift
+++ Sentimizer/View/ActivityDetailView.swift
@@ -25,151 +25,158 @@ struct ActivityDetailView: View {
     @State var isPresentingConfirm = false
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
-                Text(time)
-                    .font(.senti(size: 23))
-                    .padding(.leading, 25)
-                
+        ScrollViewReader { scrollView in
+            ScrollView {
                 VStack(alignment: .leading) {
-                    HStack {
-                        Text("ACTIVITY")
-                            .font(.senti(size: 12))
-                        Spacer()
-                    }
+                    Text(time)
+                        .font(.senti(size: 23))
+                        .padding(.leading, 25)
                     
-                    HStack {
-                        Image(systemName: icon)
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("ACTIVITY")
+                                .font(.senti(size: 12))
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            Image(systemName: icon)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(maxWidth: 45, maxHeight: 45)
+                            Text(activity)
+                                .font(.senti(size: 30))
+                        }
+                        .padding(.bottom)
+                        .gradientForeground()
+                        
+                        Text("MOOD")
+                            .font(.senti(size: 12))
+                            .padding(.top, 5)
+                        
+                        let sentiIndex = K.sentimentsArray.firstIndex(of: sentiment)
+                        Image(K.sentimentsArray[sentiIndex ?? 0])
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: 45, maxHeight: 45)
-                        Text(activity)
-                            .font(.senti(size: 30))
-                    }
-                    .padding(.bottom)
-                    .gradientForeground()
-                    
-                    Text("MOOD")
-                        .font(.senti(size: 12))
-                        .padding(.top, 5)
-                    
-                    let sentiIndex = K.sentimentsArray.firstIndex(of: sentiment)
-                    Image(K.sentimentsArray[sentiIndex ?? 0])
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 55)
-                        .padding(10)
-                        .background(RoundedRectangle(cornerRadius: 15).foregroundColor(K.sentimentColors[sentiIndex ?? 0].opacity(0.2)))
-                        .padding(.top, 5)
-                        .padding(.bottom)
-                    
-                    
-                    HStack(alignment: .top) {
-                        VStack(alignment: .leading) {
-                            Text("DESCRIPTION")
-                                .font(.senti(size: 12))
-                                .padding(.top, 5)
-                                .lineLimit(15)
-                            
-                            Group {
-                                if isEditingDescription {
-                                    ZStack(alignment: .topLeading) {
-                                        if description.isEmpty {
-                                            Text("Describe your activity and how you feel now...")
-                                                .font(.senti(size: 15))
-                                                .opacity(0.5)
-                                                .padding(7)
-                                        }
-                                        
-                                        TextEditor(text: $description)
-                                            .frame(height: 150)
-                                            .font(.senti(size: 15))
-                                            .onAppear {
-                                                UITextView.appearance().backgroundColor = .clear
+                            .frame(width: 55)
+                            .padding(10)
+                            .background(RoundedRectangle(cornerRadius: 15).foregroundColor(K.sentimentColors[sentiIndex ?? 0].opacity(0.2)))
+                            .padding(.top, 5)
+                            .padding(.bottom)
+                        
+                        
+                        HStack(alignment: .top) {
+                            VStack(alignment: .leading) {
+                                Text("DESCRIPTION")
+                                    .font(.senti(size: 12))
+                                    .padding(.top, 5)
+                                    .lineLimit(15)
+                                    .id(1)
+                                
+                                Group {
+                                    if isEditingDescription {
+                                        ZStack(alignment: .topLeading) {
+                                            if description.isEmpty {
+                                                Text("Describe your activity and how you feel now...")
+                                                    .font(.senti(size: 15))
+                                                    .opacity(0.5)
+                                                    .padding(7)
                                             }
                                             
+                                            TextEditor(text: $description)
+                                                .frame(height: 150)
+                                                .font(.senti(size: 15))
+                                                .onAppear {
+                                                    UITextView.appearance().backgroundColor = .clear
+                                                }
+                                                .onTapGesture {
+                                                    withAnimation {
+                                                        scrollView.scrollTo(1, anchor: .top)
+                                                    }
+                                                }
+                                        }
+                                        .padding()
+                                        .background(RoundedRectangle(cornerRadius: 25).foregroundColor(.gray.opacity(0.3)))
+                                    } else {
+                                        Text(description.isEmpty ? "Describe your activity..." : description)
+                                            .font(.senti(size: 18))
+                                            .padding(.bottom)
+                                            .opacity(description.isEmpty ? 0.5 : 1)
                                     }
-                                    .padding()
-                                    .background(RoundedRectangle(cornerRadius: 25).foregroundColor(.gray.opacity(0.3)))
-                                } else {
-                                    Text(description.isEmpty ? "Describe your activity..." : description)
-                                        .font(.senti(size: 18))
-                                        .padding(.bottom)
-                                        .opacity(description.isEmpty ? 0.5 : 1)
+                                }
+                                .padding(.top, 1)
+                            }
+                            Spacer()
+                            if isEditingDescription {
+                                Button {
+                                    dismissKeyboard()
+                                    updateActivityDescription(with: description)
+                                    withAnimation(.easeOut) {
+                                        isEditingDescription = false
+                                    }
+                                } label: {
+                                    Text("Done")
+                                        .bold()
+                                }
+                            } else {
+                                Button {
+                                    withAnimation(.easeOut) {
+                                        isEditingDescription = true
+                                    }
+                                } label: {
+                                    Image(systemName: "pencil")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(height: 20)
+                                        .padding(13)
+                                        .standardBackground()
                                 }
                             }
-                            .padding(.top, 1)
                         }
+                    }
+                    .padding()
+                    .standardBackground()
+                    .padding(.horizontal, 15)
+                    
+                    Button {
+                        isPresentingConfirm = true
+                    } label: {
+                        Text("Delete this activity")
+                            .font(.senti(size: 20))
+                            .foregroundColor(.red)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(RoundedRectangle(cornerRadius: 25).foregroundColor(K.brandColor1).opacity(0.1))
+                            .padding()
+                            .padding(.top)
+                    }
+                }
+            }
+            .confirmationDialog("Are you sure?", isPresented: $isPresentingConfirm) {
+                Button("Delete activity", role: .destructive) {
+                    deleteActivity(moc: viewContext)
+                    dismiss()
+                }
+            } message: {
+                Text("You cannot undo this action.")
+            }
+            .navigationTitle(day)
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    HStack {
                         Spacer()
-                        if isEditingDescription {
-                            Button {
-                                dismissKeyboard()
-                                updateActivityDescription(with: description)
-                                withAnimation(.easeOut) {
-                                    isEditingDescription = false
-                                }
-                            } label: {
-                                Text("Done")
-                                    .bold()
-                            }
-                        } else {
-                            Button {
-                                withAnimation(.easeOut) {
-                                    isEditingDescription = true
-                                }
-                            } label: {
-                                Image(systemName: "pencil")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(height: 20)
-                                    .padding(13)
-                                    .standardBackground()
+                        Button("Done") {
+                            dismissKeyboard()
+                            updateActivityDescription(with: description)
+                            withAnimation(.easeOut) {
+                                isEditingDescription = false
                             }
                         }
+                        .font(.senti(size: 19))
+                        .foregroundColor(K.brandColor2)
                     }
                 }
-                .padding()
-                .standardBackground()
-                .padding(.horizontal, 15)
-                
-                Button {
-                    isPresentingConfirm = true
-                } label: {
-                    Text("Delete this activity")
-                        .font(.senti(size: 20))
-                        .foregroundColor(.red)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(RoundedRectangle(cornerRadius: 25).foregroundColor(K.brandColor1).opacity(0.1))
-                        .padding()
-                        .padding(.top)
-                }
-            }
         }
-        .confirmationDialog("Are you sure?", isPresented: $isPresentingConfirm) {
-            Button("Delete activity", role: .destructive) {
-                deleteActivity(moc: viewContext)
-                dismiss()
-            }
-        } message: {
-            Text("You cannot undo this action.")
-        }
-        .navigationTitle(day)
-        .toolbar {
-            ToolbarItemGroup(placement: .keyboard) {
-                HStack {
-                    Spacer()
-                    Button("Done") {
-                        dismissKeyboard()
-                        updateActivityDescription(with: description)
-                        withAnimation(.easeOut) {
-                            isEditingDescription = false
-                        }
-                    }
-                    .font(.senti(size: 19))
-                    .foregroundColor(K.brandColor2)
-                }
-            }
         }
     }
     
