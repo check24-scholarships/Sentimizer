ed8a1529f72e7282a9737c894fa344562aa06cbb
Samuel <saginsberg@icloud.com>
2022-05-14 16:41:42 +0200

    start activity detail view

--- Sentimizer/View/ActivityDetailView.swift
+++ Sentimizer/View/ActivityDetailView.swift
@@ -2,7 +2,57 @@
 //  ActivityDetailView.swift
 //  Sentimizer
 //
-//  Created by Justin Hohenstein on 14.05.22.
+//  Created by Samuel Ginsberg on 13.05.22.
 //
 
-import Foundation
+import SwiftUI
+
+struct ActivityDetailView: View {
+    
+    let activity: String
+    let icon: String
+    let sentiment: String
+    let description: String
+    let date: Date
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Group {
+                    Text(DataController.formatDate(date: date, format: "EEE, d MMM"))
+                        .font(.senti(size: 20))
+                    Text(DataController.formatDate(date: date, format: "HH:mm"))
+                        .font(.senti(size: 18))
+                }
+                .padding(.vertical)
+                    
+                HStack {
+                    Image(systemName: icon)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 45)
+                    Text(activity)
+                        .font(.senti(size: 23))
+                        .padding(.leading)
+                }
+                .padding(.vertical)
+                
+                Image(K.sentimentsArray.filter { $0 == sentiment }[0])
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 55)
+                    .gradientForeground()
+                
+                Text(description)
+                    .font(.senti(size: 18))
+            }
+            .padding(.horizontal, 15)
+        }
+    }
+}
+
+struct ActivityDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityDetailView(activity: "Walking", icon: "figure.walk", sentiment: "content", description: "Helloooooo im walking yesss so much fun", date: .now)
+    }
+}
