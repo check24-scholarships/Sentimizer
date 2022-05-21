1af9079b396d71fde8d0b9b7f3a9d3813642c4cf
JustGitEverything <85348070+JustGitEverything@users.noreply.github.com>
2022-05-21 10:40:37 +0200

    move mlmodel actions to datacontroller

--- Sentimizer/Model/DataController.swift
+++ Sentimizer/Model/DataController.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import CoreData
+import CoreML
 
 class DataController: ObservableObject {
     private static var container: NSPersistentContainer {
@@ -18,6 +19,8 @@ class DataController: ObservableObject {
         })
         return container
     }
+    
+    var defaults = UserDefaults.standard
     
     var context: NSManagedObjectContext {
         return DataController.container.viewContext
@@ -368,5 +371,67 @@ class DataController: ObservableObject {
         }
         
         return ((["Soccer"], [0.5]), (["Project"], [-0.2]))
+    }
+    
+    func getModel() {
+        let resourceDocPath = (FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)).last! as URL
+        let modelName = "TestModel.mlmodelc"
+        let actualPath = resourceDocPath.appendingPathComponent(modelName)
+        
+        guard let url = URL(string: "http://127.0.0.1:8000/") else { return }
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { [self] (data, response, error) in
+            guard let data = data else { return }
+            do {
+                try data.write(to: actualPath)
+                
+                print(actualPath)
+                
+                let model = try MLModel.compileModel(at: actualPath)
+                
+                print("pLs", model)
+                
+                let permanentURL = try FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent(model.lastPathComponent)
+                _ = try FileManager.default.replaceItemAt(permanentURL, withItemAt: model)
+                
+                self.defaults.set(permanentURL.absoluteString, forKey: K.modelURL)
+                
+                print("absolute", permanentURL, permanentURL.absoluteString)
+            } catch let error {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+    
+    func feedforward(ip: [Double]) -> [Double] {
+        do {
+            let permanentURL = self.defaults.object(forKey: K.modelURL)
+            let model = try TestModel(contentsOf: URL(string: permanentURL as! String)!)
+            
+            let mlMultiArrayInput = try? MLMultiArray(shape:[1, NSNumber(value: ip.count)], dataType:MLMultiArrayDataType.double)
+            
+            for i in 0 ..< ip.count {
+                mlMultiArrayInput![i] = ip[i] as NSNumber
+            }
+            
+            print("MLI", mlMultiArrayInput as Any)
+            
+            let output = try model.prediction(input: TestModelInput(ip: mlMultiArrayInput!)).var_6
+            
+            print(output)
+            
+            var result:[Double] = []
+            
+            for i in 0 ..< output.count {
+                result.append(Double(truncating: output[i]))
+            }
+            
+            return result
+        } catch let error {
+            print(error)
+        }
+        
+        return []
     }
 }
